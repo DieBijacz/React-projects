c7508bd5507a9ac7585862634e433cff50e9011c
masta <mastanilewicz@gmail.com>
2021-12-25 14:53:43 +0000

    update

--- recipe-app/src/components/Header.js
+++ recipe-app/src/components/Header.js
@@ -4,13 +4,13 @@ import getRandomRecipe from '../fetch.js'
 export const Header = () => {
     return (
         <header>
-        <form className="search-form">
-          <input className="search-bar" type="text" placeholder="Search for recipe" />
-          <button onClick={(e) => {
-            e.preventDefault()
-            getRandomRecipe()
-          }} className="btn search-btn" type="submit">Search</button>
-        </form>
-      </header>
+            <form className="search-form">
+                <input className="search-bar" type="text" placeholder="Search for recipe" />
+                <button onClick={(e) => {
+                    e.preventDefault()
+                    getRandomRecipe()
+                }} className="btn search-btn" type="submit">Search</button>
+            </form>
+        </header>
     )
 }
