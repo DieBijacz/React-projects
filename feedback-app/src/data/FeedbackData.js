1ab3b019f61377f173fc28a58d2747cfdb585d38
masta <mastanilewicz@gmail.com>
2021-12-25 23:14:16 +0000

    commit update

---
+++ feedback-app/src/data/FeedbackData.js
@@ -0,0 +1,17 @@
+export const feedbackData = [
+  {
+    id: 1,
+    rating: 9,
+    text: "makarena",
+  },
+  {
+    id: 2,
+    rating: 4,
+    text: "wiosło masło zgasło",
+  },
+  {
+    id: 3,
+    rating: 7,
+    text: "bijcie masterczulki",
+  },
+];
